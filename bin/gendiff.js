ddc12da95578da278f48b06e6b61e870922cb796
Петр Ломаев <petrucho060395@gmail.com>
2023-11-30 20:42:37 +0400

    files step 2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,14 +2,12 @@
 import { program } from 'commander';
 
 program
-  .name('gendiff')
+  //.name('gendiff')
+  .version('0.0.1')
+  .arguments('<filepath1>', '<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
   .option('-h, --help', 'output usage information')
-  .argument('<filepath1>')
-  .argument('<filepath2>')
-  .option('-f, --format <type>', 'output format');
-
-program.command('gendiff');
-
-program.parse();
+  .option('-f, --format <type>', 'output format')
+  .action('')
+  .parse(process.argv);
